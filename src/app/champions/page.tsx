5cb21151739f27488cadb75d9ef5173873f3fb6a
woohyuckk <woohyuckk@gmail.com>
2025-03-12 00:00:57 +0900

    style: 챔피언 목록 페이지 변경

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -11,26 +11,30 @@ import Link from "next/link";
 const Champions = async () => {
   const championsArr: Champion[] = await fetchChampionList();
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-4 p-8">
-      {championsArr.map((champion) => {
-        return (
-          <div key={champion.key} className="border-2">
-            <Link href={`/champions/${champion.id}`}>
-              <Image
-                priority
-                src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.image.full}`}
-                alt="Profile of Champion"
-                width={150}
-                height={150}
-                className="w-full object-cover"
-              />
-              <h1>{champion.name}</h1>
-              <p>{champion.title}</p>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+    <section className="px-10 py-20 lg:px-20 text-white">
+      <h1 className="px-10 font-bold text-4xl lg:text-6xl lg:text-center text-gold-heavy  ">
+        챔피언 목록
+      </h1>
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-4 p-8 items-center justify-start">
+        {championsArr.map((champion) => {
+          return (
+            <div key={champion.key} className="border-2 border-gold-light rounded-sm">
+              <Link href={`/champions/${champion.id}`}>
+                <Image
+                  priority
+                  src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/champion/${champion.image.full}`}
+                  alt={champion.name}
+                  width={150}
+                  height={150}
+                  className="w-full object-cover"
+                />
+                <h1 className="text-center font-bold bg-black">{champion.name}</h1>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
