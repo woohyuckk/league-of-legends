b8f6a129d6b4f92785cbf2c8ae445baf3e801cdd
woohyuckk <woohyuckk@gmail.com>
2025-03-11 15:49:07 +0900

    feat: champion data type 재정의 (#4)

--- src/types/champions.ts
+++ src/types/champions.ts
@@ -6,20 +6,49 @@ export interface ChampionData {
 
 // 챔피언 추출 데이터
 export interface Champion {
-  id: string;
-  key: string;
-  name: string;
-  title: string;
-  blurb: string;
-  image: {
-    full: string;
-    sprite: string;
-    group: string;
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-  };
+  version: Version;
+  id:      string;
+  key:     string;
+  name:    string;
+  title:   string;
+  blurb:   string;
+  info:    Info;
+  image:   Image;
+  tags:    Tag[];
+  partype: string;
+  stats:   { [key: string]: number };
+}
+
+export interface Image {
+  full:   string;
+  sprite: string;
+  group:  string;
+  x:      number;
+  y:      number;
+  w:      number;
+  h:      number;
+}
+
+
+
+export interface Info {
+  attack:     number;
+  defense:    number;
+  magic:      number;
+  difficulty: number;
+}
+
+export enum Tag {
+  Assassin = "Assassin",
+  Fighter = "Fighter",
+  Mage = "Mage",
+  Marksman = "Marksman",
+  Support = "Support",
+  Tank = "Tank",
+}
+
+export enum Version {
+  The1551 = "15.5.1",
 }
 
 export interface ChampionDetail {
