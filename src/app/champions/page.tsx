93ad102ce8244aa91025d478d08df1d03216f6dc
woohyuckk <woohyuckk@gmail.com>
2025-03-11 19:02:44 +0900

    chore: 챔피언명 영문->한글 변경 (#4)

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 const Champions = async () => {
   const championsArr: Champion[] = await fetchChampionList();
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-4">
+    <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-4 p-8">
       {championsArr.map((champion) => {
         return (
           <div key={champion.key} className="border-2">
@@ -24,7 +24,7 @@ const Champions = async () => {
                 height={150}
                 className="w-full object-cover"
               />
-              <h1>{champion.id}</h1>
+              <h1>{champion.name}</h1>
               <p>{champion.title}</p>
             </Link>
           </div>
