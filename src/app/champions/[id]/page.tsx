c657c3dab5e42161d0bf62e109d1c31e3b441d35
woohyuckk <woohyuckk@gmail.com>
2025-03-12 19:33:31 +0900

    style: champion detail 스타일 변경

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -23,40 +23,61 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 const ChampionDetailPage = async ({ params }: Props) => {
   const [champion]: ChampionDetail[] = await fetchChampionDetail(params.id);
   return (
-    <div className="text-white">
-      <h1>{champion.id}</h1>
-      <p>{champion.name}</p>
-      <p>{champion.title} </p>
-      <Image
-        src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`}
-        alt="champion illustation"
-        width={100}
-        height={100}
-        className="object-contain"
-      />
-      <Image
-      src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/passive/${champion.passive.image.full}`}
-      alt="Champion Passive"
-      width={100}
-      height={100}
+    <div className="flex flex-col items-center text-white p-6">
+      {/* 챔피언 메인 정보 */}
+      <div className="flex flex-col md:flex-row w-full max-w-4xl items-center gap-6">
+        <Image
+          src={`${RIOT_URL.CHAMPION_SPLASH}/${champion.id}_0.jpg`}
+          alt="champion illustration"
+          width={500}
+          height={300}
+          className="object-cover rounded-lg border border-gray-500"
+        />
+        <div className="flex flex-col text-left text-white">
+          <h1 className="text-4xl font-bold">{champion.name}</h1>
+          <p className="text-xl font-semibold text-gray-300">{champion.title}</p>
+          <p className="mt-2 text-sm text-white">{champion.blurb}</p>
+        </div>
+      </div>
 
-      />
-      <span className="block">{champion.passive.name}</span>
-      <span>{champion.passive.description}</span>
-      {champion.spells.map((spell) => {
-        return (
-          <React.Fragment key={spell.id}>
+      {/* 스킬 섹션 */}
+      <div className="mt-8 w-full max-w-4xl flex flex-col items-center">
+        <div className="flex  flex-wrap w-full justify-start gap-6 items-center">
+          {/* 패시브 */}
+          <div className="flex flex-col items-center text-center relative group">
             <Image
-              src={`${RIOT_URL.CHAMPION_SPELLS}/${spell.id}.png`}
-              alt="Q skill"
-              width={100}
-              height={100}
+              src={`${RIOT_URL.CHAMPION_PASSIVE}/${champion.passive.image.full}`}
+              alt="Champion Passive"
+              width={80}
+              height={80}
+              className="rounded-lg border border-gray-500"
             />
-            <span className="block">{spell.name}</span>
-            <span>{spell.description}</span>
-          </React.Fragment>
-        );
-      })}
+            <span className="mt-2 font-semibold">{champion.passive.name}</span>
+            <div className="absolute top-full left-1/2 -translate-x-1/2 hidden group-hover:flex w-64 p-2 bg-black text-white text-sm rounded-lg shadow-lg mt-2 z-10">
+              {champion.passive.description}
+            </div>
+          </div>
+
+          {/* 일반 스킬들 */}
+          <div className="flex gap-4 relative flex-wrap justify-center">
+            {champion.spells.map((spell) => (
+              <div key={spell.id} className="group flex flex-col items-center relative">
+                <Image
+                  src={`${RIOT_URL.CHAMPION_SPELLS}/${spell.id}.png`}
+                  alt={spell.name}
+                  width={80}
+                  height={80}
+                  className="rounded-lg border border-gray-500"
+                />
+                <span className="mt-2 font-semibold">{spell.name}</span>
+                <div className="absolute top-full left-1/2 -translate-x-1/2 hidden group-hover:flex w-64 p-2 bg-black text-white text-sm rounded-lg shadow-lg mt-2 z-10">
+                  {spell.description}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/cosntants/api.ts
+++ src/cosntants/api.ts
@@ -1,10 +1,12 @@
-
-
-
-
 export const RIOT_URL = {
   VERSION_API: "https://ddragon.leagueoflegends.com/api/versions.json",
-  CHAMPIONS_API: (version: string) => `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR`,
-  ROTATION_CHAMPIONS: "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
-  CHAMPION_SPELLS :"https://ddragon.leagueoflegends.com/cdn/15.5.1/img/spell"
-}
+  CHAMPIONS_API: (version: string) =>
+    `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR`,
+  ROTATION_CHAMPIONS:
+    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+  CHAMPION_SPELLS: "https://ddragon.leagueoflegends.com/cdn/15.5.1/img/spell",
+  CHAMPION_SPLASH:
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/splash",
+  CHAMPION_PASSIVE:
+    "https://ddragon.leagueoflegends.com/cdn/15.5.1/img/passive",
+};
