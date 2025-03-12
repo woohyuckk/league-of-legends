b850f75de855b4464ef1510f1b75c3e324e1bd79
woohyuckk <woohyuckk@gmail.com>
2025-03-12 15:43:45 +0900

    feat: rotation route handler 선언

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,20 +1,26 @@
+import { RIOT_URL } from "@/cosntants/api";
+import { fetchChampionList } from "@/utils/serverApi";
+
 export async function GET() {
-  const rotationRes = await fetch(
-    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
-    {
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
-        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": `${process.env.RIOT_API_KEY}`
+  const rotationRes = await fetch(`${RIOT_URL.ROTATION_CHAMPIONS}`, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+      "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+      Origin: "https://developer.riotgames.com",
+      "X-Riot-Token": `${process.env.RIOT_API_KEY}`,
     },
-    }
+  });
+
+  let rotationData = await rotationRes.json();
+  const chmapionListData = await fetchChampionList();
+
+  rotationData = rotationData.freeChampionIds;
+
+  const rotationChampions = rotationData.map((championId: number) =>
+    chmapionListData.find((champion) => Number(champion.key) === championId)
   );
 
-  const chmapionListRes= await fetch()
-
-
-
-  return Response.json({ data });
+  return Response.json(rotationChampions);
 }
--- src/cosntants/api.ts
+++ src/cosntants/api.ts
@@ -3,7 +3,7 @@
 
 
 export const RIOT_URL = {
-  VERSION_API : "https://ddragon.leagueoflegends.com/api/versions.json",
+  VERSION_API: "https://ddragon.leagueoflegends.com/api/versions.json",
   CHAMPIONS_API: (version: string) => `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR`,
-  
+  ROTATION_CHAMPIONS: "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations"
 }
