c3ef7d3240d544895931abdcbb4ffdce75a5d8e6
woohyuckk <woohyuckk@gmail.com>
2025-03-12 20:09:02 +0900

    style: item 목록 ui 변경

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,32 +1,21 @@
+import ItemCard from "@/components/items/ItemCard";
 import { Item } from "@/types/items";
 import { fetchItemList } from "@/utils/serverApi";
-import Image from "next/image";
-import Link from "next/link";
 
 const Items = async () => {
   const items: [string, Item][] = await fetchItemList();
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-4 p-8 text-white">
-      {items.map((item) => {
-        return (
-          <div key={item[0]} className="border-2">
-            <Link href={`/items/${item[0]}`}>
-              <Image
-                priority
-                src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${item[0]}.png`}
-                alt="item illustation"
-                width={150}
-                height={150}
-                className="w-full object-cover"
-              />
-              <div> {item[1].name}</div>
-              <div> {item[1].plaintext}</div>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+    <section className="px-10 py-20 lg:px-20 text-white">
+      <h1 className="px-10 font-bold text-4xl lg:text-6xl lg:text-center text-gold-heavy  ">
+        아이템 목록
+      </h1>
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] gap-4 p-8 items-center justify-start">
+        {items.map((item) => {
+          return <ItemCard key={item[0]} item={item} />;
+        })}
+      </div>
+    </section>
   );
 };
 
---
+++ src/components/items/ItemCard.tsx
@@ -0,0 +1,37 @@
+import { Gold, Item } from "@/types/items";
+import Image from "next/image";
+import Link from "next/link";
+import { FaCoins } from "react-icons/fa";
+
+// tuple 타입 정의
+interface ItemCardProps {
+  item: [string, Item];
+}
+
+const ItemCard = ({ item }: ItemCardProps) => {
+  const gold: Gold = item[1].gold;
+
+  return (
+    <div key={item[0]} className="border-2 border-gold-light rounded-sm">
+      <Link href={`/items/${item[0]}`}>
+        <Image
+          priority
+          src={`https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${item[0]}.png`}
+          alt="item illustation"
+          width={150}
+          height={150}
+          className="w-full object-cover"
+        />
+        <span className="block text-center font-bold bg-black">
+          {item[1].name}
+        </span>
+
+        <span className="flex justify-center text-center font-bold bg-black">
+          <FaCoins className="text-gold-heavy" /> {gold.total}
+        </span>
+      </Link>
+    </div>
+  );
+};
+
+export default ItemCard;
