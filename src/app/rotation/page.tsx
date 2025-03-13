15bc092560d632f3927afd709d8c1dbc9a8c02ec
woohyuckk <woohyuckk@gmail.com>
2025-03-13 14:21:37 +0900

    style: rotation champion ui

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -3,6 +3,8 @@
 import { getChampionRotation } from "@/utils/riotApi";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "../loading";
+import ChampionCard from "@/components/champions/ChampionCard";
+import { Suspense } from "react";
 
 const Rotation = () => {
   const {
@@ -18,7 +20,20 @@ const Rotation = () => {
   if (isError) return <div>Error...</div>;
 
   console.log(rotationChampions);
-  return <div>Rotation</div>;
+  return (
+    <div className="mx-[10%] mt-20">
+      <h1 className="text-center text-4xl font-bold text-gold-heavy lg:text-6xl">
+        이번주 로테이션
+      </h1>
+      <Suspense fallback={<Loading />}>
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] items-center justify-start gap-4 p-8 text-white">
+          {rotationChampions?.map((champion) => (
+            <ChampionCard champion={champion} key={champion.id} />
+          ))}
+        </div>
+      </Suspense>
+    </div>
+  );
 };
 
 export default Rotation;
