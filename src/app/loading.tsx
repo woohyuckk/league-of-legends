68106f7f723d202f3f6563ec5e4db4d3eb96f95e
woohyuckk <woohyuckk@gmail.com>
2025-03-13 10:56:11 +0900

    style: loading 추가

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-gold-heavy border-gray-200 border-t-gold-heavy"></div>
+    </div>
+  );
 };
 
 export default Loading;
