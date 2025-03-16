be226a45a9a03bcc05b833e63f165bd2293ac641
woohyuckk <woohyuckk@gmail.com>
2025-03-16 17:13:45 +0900

    chore: 주석 추가

--- src/app/rotation/RotationList.tsx
+++ src/app/rotation/RotationList.tsx
@@ -2,20 +2,19 @@
 
 import { getChampionRotation } from "@/utils/riotApi";
 import { useQuery } from "@tanstack/react-query";
-import Loading from "../loading";
 import ChampionCard from "@/components/champions/ChampionCard";
 
+/**
+ * isLoading은 suspense로 fetch 발생하는 promise를 catch하여 Loading을 처리해주고 있기 때문에 불필요하다고 생각하여 제거하였습니다.
+ * getChampionRotation: Route handler를 이용한 호출방식입니다.
+ */
+
 const RotationList = () => {
-  const {
-    data: rotationChampions,
-    isLoading,
-    isError,
-  } = useQuery({
+  const { data: rotationChampions, isError } = useQuery({
     queryKey: ["rotation"],
     queryFn: getChampionRotation,
   });
 
-  if (isLoading) return <Loading />;
   if (isError) return <div>Error...</div>;
   return (
     <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] items-center justify-start gap-4 p-8 text-white">
--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -8,15 +8,18 @@ import {
 import { getRotationList } from "@/utils/serverApi";
 import RotationList from "./RotationList";
 
+/**
+ * 기존에 만들어 놓았던 getChampionRotation(route handler)함수를 재활용하려고 했으나, 서버에서 호출하면 error가 발생
+ * getRotationList라는 server action 함수를 따로 분리하여 호출하였습니다.
+ */
+
 const Rotation = async () => {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ["rotation"],
-    queryFn: () => getRotationList(),
+    queryFn: getRotationList,
   });
-  console.log("rotation page")
-
   return (
     <div className="mx-[10%] mt-20">
       <h1 className="text-center text-4xl font-bold text-gold-heavy lg:text-6xl">
