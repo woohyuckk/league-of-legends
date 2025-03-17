27fd5070993ac67c98ff5fed699b5894b6767bec
woohyuckk <woohyuckk@gmail.com>
2025-03-17 14:29:29 +0900

    restore: Loading 복구

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-gold-heavy border-gray-200 border-t-gold-heavy"></div>
+    </div>
+  );
+};
+
+export default Loading;
