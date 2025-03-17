0b4d5fec31b84d4dcc5c42cdcf1c9e643045c78b
woohyuckk <woohyuckk@gmail.com>
2025-03-17 18:58:28 +0900

    refact: API 환경변수 변경

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -64,7 +64,7 @@ export const getRotationList = async () => {
       "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
       "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
       Origin: "https://developer.riotgames.com",
-      "X-Riot-Token": `${process.env.NEXT_PUBLIC_RIOT_API_KEY}`,
+      "X-Riot-Token": `${process.env.RIOT_API_KEY}`,
     },
   });
 
